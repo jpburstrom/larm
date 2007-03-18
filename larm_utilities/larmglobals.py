3dfed8bb5ef56c227ee5275774eb1e664ef0837d
johannes <johannes@57a3cfcf-4f2b-0410-9743-9d09f4b2abc6>
2007-03-18 14:00:14 +0000

    fixed path to backend

--- larm_utilities/larmglobals.py
+++ larm_utilities/larmglobals.py
@@ -5,7 +5,7 @@ from qt import QColor, QAction, QKeySequence
 def getgl(key):
     
     gl = {
-        "pdcommand" : "pd -nogui -rt -cb_scheduler /home/johannes/pd/larm/immer2-back.pd", #pd launch command
+        "pdcommand" : "".join(["pd -nogui -rt -cb_scheduler", sys.path[0],  "/backend/immer2-back.pd"]), #pd launch command
         "polltime" : 0.02, #ms, how often mouse gets polled
         "mouse_resolution" : 3000, #a factor, larger = faster (i think)
         "osc_address" : "127.0.0.1", 
